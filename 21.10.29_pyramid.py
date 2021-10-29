a054b8c3dc1b4ab7c6a69a9ec304e1f9777b3210
junkyu lee <86291408+junkyu92@users.noreply.github.com>
2021-10-29 18:15:43 +0900

    Update 21.10.29_pyramid.py

--- 21.10.29_pyramid.py
+++ 21.10.29_pyramid.py
@@ -2,8 +2,6 @@
 # UTF-8 encoding when using korean
 input = int(input())
 for i in range(input-1, -1, -1):
-	for j in range(0, i):
-		print(" ", end="")
-	for j in range(0, 2*(input-i)-1):
-		print("*", end="")
+	print(" "*i, end="")
+	print("*"*(2*(input-i)-1), end="")
 	print("")
