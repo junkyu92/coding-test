a2589e1fc48dcdbd9214ee340372060a6d4663ec
junkyu lee <86291408+junkyu92@users.noreply.github.com>
2021-11-01 22:42:47 +0900

    Update 21.11.02_brokenComputer.java

--- 21.11.02_brokenComputer.java
+++ 21.11.02_brokenComputer.java
@@ -1,38 +1,22 @@
 import java.io.*;
 import java.util.*;
 class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		String in1 = sc.nextLine();
-		String[] in11 = in1.split(" ");
-		int first = Integer.parseInt(in11[0]);
-		int second = Integer.parseInt(in11[1]);
-		while(!(1<=first) || !(first<=Math.pow(10, 5)) || !(1<=second) || !(second<=Math.pow(10, 9))){
-			in1 = sc.nextLine();
-			in11 = in1.split(" ");
-			first = Integer.parseInt(in11[0]);
-			second = Integer.parseInt(in11[1]);
-		}
-		
-		String in2 = sc.nextLine();
-		String[] in22 = in2.split(" ");
-		int[] in222 = new int[in22.length];
-		for (int i = 0; i < in22.length; i++){
-			in222[i] = Integer.parseInt(in22[i]);
-			while(!(1<=in222[i]) || !(in222[i]<=Math.pow(10,9))){
-				in2 = sc.nextLine();
-				in22 = in2.split(" ");
-				in222[i] = Integer.parseInt(in22[i]);
-			}
-		}
+	public static int broken(String[] in1, String[] in2){
 		int sum = 1;
-		for(int i = first-1; i >= 1; i--){
-			if(in222[i]-in222[i-1] <= second){
+		for(int i = Integer.parseInt(in1[0])-1; i >= 1; i--){
+			if(Integer.parseInt(in2[i])-Integer.parseInt(in2[i-1]) <= Integer.parseInt(in1[1])){
 				sum++;
 			}else{
 				break;
 			}
 		}
-		System.out.print(sum);
+		return sum;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String[] in1 = sc.nextLine().split(" ");
+		String[] in2 = sc.nextLine().split(" ");
+		System.out.print(broken(in1, in2));
 	}
 }
