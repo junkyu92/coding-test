f6d50448fa38e5d7c2a3fea2f9684beeaa59b167
junkyu lee <86291408+junkyu92@users.noreply.github.com>
2021-11-01 22:29:49 +0900

    Update 21.11.01_ram.java

--- 21.11.01_ram.java
+++ 21.11.01_ram.java
@@ -4,9 +4,6 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		while (!(1<=n) || !(n<=100)){
-			n = sc.nextInt();
-		}
 		sc.nextLine();
 		String m = sc.nextLine();
 		String[] m2 = m.split(" ");
@@ -14,14 +11,7 @@ class Main {
 		int sum = n;
 		String result = "";
 		for (int i = 0; i < m2.length; i++){
-			if(!(1<=m2.length) || !(m2.length<=n) || !(100<=m3[i]) || !(m3[i]<=Math.pow(2,31)-1)){
-				m = sc.nextLine();
-				String[] m2 = m.split(" ");
-				int[] m3 = new int[m2.length];
-			}
-		for (int i = 0; i < m2.length; i++){
 			m3[i] = Integer.parseInt(m2[i]);
-			
 			boolean t = true;
 			result = result + Integer.toString(i+1) + " ";
 			for(int j =1; j<31; j++){
