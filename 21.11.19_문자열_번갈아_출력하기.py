8037b9c2ff7f1f06f1f3f0245ae293331acc6fe0
junkyu lee <86291408+junkyu92@users.noreply.github.com>
2021-11-19 15:24:55 +0900

    Update 21.11.19_문자열_번갈아_출력하기.py

--- 21.11.19_문자열_번갈아_출력하기.py
+++ 21.11.19_문자열_번갈아_출력하기.py
@@ -1,7 +1,7 @@
 txt = input()
 txt2 = ""
 for i in range(int(len(txt)/2)):
-	txt2 += txt[i] + txt[len(txt)-1-i]
+	txt2 += txt[i] + txt[-1-i]
 if len(txt)%2 != 0:
 	txt2 += txt[int((len(txt)-1)/2)]
 print(txt2)
