c1f243bc183fba35c3ac571e82b032c13d054a2d
junkyu lee <86291408+junkyu92@users.noreply.github.com>
2023-12-12 17:16:13 +0900

    [level 2] Title: 주식가격, Time: 25.12 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 24.19 ms
+메모리: 67.6 MB, 시간: 25.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 16:48:22
+2023년 12월 2일 17:16:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,21 +1,30 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        //100,000 제곱 = 10,000,000,000 100초 X
-        
-        //두번째 탐색범위 줄어드는거 감안해도 
-        //100,000 ~ 1 더하기
-        //100,000 * 100,001 / 2 = 5,000,050,000 50초?
-        
-        //break 때문에 통과되는 것 같은데 
-        //시간복잡도 최악으로 계산하면 50초라 실패해야되지 않나
+        //스택풀이
+        Stack<Price> stack = new Stack<>();
         for(int i = 0; i < prices.length; i++){
-            int target = prices[i];
-            for(int j = i+1; j < prices.length; j++){
-                answer[i]++;
-                if(prices[j] < target) break;
+            answer[i] = prices.length - i - 1;
+            Price price = new Price(prices[i], i);
+            if(stack.isEmpty()) stack.push(price);
+            else{
+                while(!stack.isEmpty() && stack.peek().price > prices[i]){
+                    answer[stack.peek().idx] = i - stack.pop().idx;
+                }
+                stack.push(price);
             }
         }
         return answer;
+    }
+}
+
+class Price{
+    int price;
+    int idx;
+    
+    Price(int price, int idx){
+        this.price = price;
+        this.idx = idx;
     }
 }
